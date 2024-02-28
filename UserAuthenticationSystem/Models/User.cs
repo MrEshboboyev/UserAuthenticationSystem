1886f8ccc52d347f16fa508f4a43aef0bfdb19c5
MrEshboboyev <mreshboboyev@gmail.com>
2024-02-28 19:45:47 +0500

    Create a User.cs model

---
+++ UserAuthenticationSystem/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace UserAuthenticationSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ProfilePicture { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
