015407213adca8b527c0e46e8bbf41844b456062
MrEshboboyev <mreshboboyev@gmail.com>
2024-02-28 20:39:13 +0500

    Create UsersController.cs and Index() action and view writed

---
+++ UserAuthenticationSystem/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAuthenticationSystem.Data;
+using UserAuthenticationSystem.Models;
+
+namespace UserAuthenticationSystem.Controllers
+{
+    public class UsersController : Controller
+    {
+        private AppDbContext _context;
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<User> objList = _context.Users.ToList(); 
+            return View(objList);
+        }
+    }
+}
